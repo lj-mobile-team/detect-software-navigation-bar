a8c915ac1d888e2f10d595ccc7e781176fc91ace
Evgeniy Kozhich <e.kozhich@rambler-co.ru>
2019-06-19 14:32:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { NativeModules } from 'react-native';
 const { RNDetectSoftwareNavigationBar } = NativeModules;
 
 const isSoftware = async () => {
-  return await RNDetectSoftwareNavigationBar.isSoftware();
+  return await RNDetectSoftwareNavigationBar.isSoftware().then(isEnabled => isEnabled).catch(() => false);
 }
 
-export default isSoftware().then(isEnabled => isEnabled).catch(() => false);
+export default isSoftware();
